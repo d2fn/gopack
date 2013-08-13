e099aca3c39710eb6ace0b479ac67192ff54b270
David Calavera <david.calavera@gmail.com>
2013-08-12 22:09:30 -0700

    Fix sub-package imports.
    This fixes a regression where it doesn't know how to checkout branch and tags for packages imported
    that are not the base of a repository.

--- model_test.go
+++ model_test.go
@@ -13,13 +13,20 @@ func setupTestPwd() {
 	setPwd()
 }
 
-func createScmDep(project, scm string) *Dep {
-	dep := &Dep{Import: project}
-	scmPath := path.Join(dep.Src(), scm)
-	err := os.MkdirAll(scmPath, 0700)
-
+func createPath(path string) {
+	err := os.MkdirAll(path, 0700)
 	if err != nil {
 		panic(err)
+	}
+}
+
+func createScmDep(scm string, project string, paths ...string) *Dep {
+	dep := &Dep{Import: project}
+	scmPath := path.Join(dep.Src(), scm)
+	createPath(scmPath)
+
+	for _, p := range paths {
+		createPath(path.Join(scmPath, p))
 	}
 
 	return dep
@@ -28,7 +35,7 @@ func createScmDep(project, scm string) *Dep {
 func TestGit(t *testing.T) {
 	setupTestPwd()
 
-	dep := createScmDep("github.com/d2fn/gopack", ".git")
+	dep := createScmDep(".git", "github.com/d2fn/gopack")
 
 	scm, err := dep.Scm()
 	if scm != "git" {
@@ -39,7 +46,7 @@ func TestGit(t *testing.T) {
 func TestHg(t *testing.T) {
 	setupTestPwd()
 
-	dep := createScmDep("code.google.com/p/go", ".hg")
+	dep := createScmDep(".hg", "code.google.com/p/go")
 
 	scm, err := dep.Scm()
 	if scm != "hg" {
@@ -50,10 +57,22 @@ func TestHg(t *testing.T) {
 func TestUnknownScm(t *testing.T) {
 	setupTestPwd()
 
-	dep := createScmDep("foo/bar/baz", ".svn")
+	dep := createScmDep(".svn", "foo/bar/baz")
 
 	scm, err := dep.Scm()
 	if err == nil {
 		t.Errorf("Expected unknown scm but it was %s", scm)
+	}
+}
+
+func TestSubPackages(t *testing.T) {
+	setupTestPwd()
+
+	dep := createScmDep(".hg", "code.google.com/p/go", "path/filepath", "io")
+	dep.Import = "code.google.com/p/go/path"
+
+	scm, err := dep.Scm()
+	if scm != "hg" {
+		t.Errorf("Expected scm to be hg but it was %s.\n%v", scm, err)
 	}
 }
