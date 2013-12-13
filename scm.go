99f90df3facb97b7af46e9df343c235f249add05
David Calavera <david.calavera@gmail.com>
2013-12-12 21:59:11 -0800

    Complete provisioners.
    - Fetch code when the dependency already exists.
    - Add subversion support.

--- scm.go
+++ scm.go
@@ -4,36 +4,75 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"os/exec"
+	"path"
 )
 
 type Scm interface {
 	Init(d *Dep) error
 	Checkout(d *Dep) error
+	Fetch(path string) error
+	DownloadCommand(source, path string) *exec.Cmd
+}
+
+func dependencyPath(importPath string) string {
+	return path.Join(pwd, VendorDir, "src", importPath)
+}
+
+func scmStageDir(depPath, scmDir string) string {
+	return path.Join(depPath, scmDir)
+}
+
+func downloadDependency(d *Dep, depPath, scmType string, scm Scm) (err error) {
+	_, err = os.Stat(scmStageDir(depPath, scmType))
+
+	if os.IsExist(err) {
+		err = scm.Fetch(depPath)
+	} else if err != nil {
+		err = fmt.Errorf("Error while examining dependency path for %s: %s", d.Import, err)
+	} else {
+		fmt.Printf("Downloading %s to %s", d.Source, depPath)
+
+		cmd := scm.DownloadCommand(d.Source, depPath)
+
+		if err = cmd.Run(); err != nil {
+			return fmt.Errorf("Error downloading dependency: %s", err)
+		}
+	}
+
+	return
+}
+
+func initScm(d *Dep, scmType string, scm Scm) error {
+	path := dependencyPath(d.Import)
+
+	if err := os.MkdirAll(path, 0755); err != nil {
+		return fmt.Errorf("Error creating import dir %s", err)
+	} else {
+		downloadDependency(d, path, scmType, scm)
+	}
+	return nil
+}
+
+func runInPath(path string, fn func() error) error {
+	err := os.Chdir(path)
+	if err != nil {
+		return err
+	}
+	defer os.Chdir(pwd)
+
+	return fn()
 }
 
 type Git struct{}
 
 func (g Git) Init(d *Dep) error {
-	path := fmt.Sprintf("%s/%s/src/%s", pwd, VendorDir, d.Import)
-	if err := os.MkdirAll(path, 0755); err != nil {
-		return fmt.Errorf("Error creating import dir %s", err)
-	} else {
-		if _, err := os.Stat(fmt.Sprintf("%s/%s", path, ".git")); os.IsNotExist(err) {
-			fmt.Printf("Cloning %s to %s", d.Source, path)
-			cmd := exec.Command("git", "clone", d.Source, path)
-			if err := cmd.Run(); err != nil {
-				return fmt.Errorf("Error cloning repo %s", err)
-			}
-		} else if err == nil {
-			log.Printf("Git dir exists for %s, skipping clone. To reset the source, run `rm -R %s`, then run gopack again", d.Import, path)
-		} else {
-			return fmt.Errorf("Error while examining git dir for %s: %s", d.Import, err)
-		}
-	}
-	return nil
+	return initScm(d, ".git", g)
+}
+
+func (g Git) DownloadCommand(source, path string) *exec.Cmd {
+	return exec.Command("git", "clone", source, path)
 }
 
 func (g Git) Checkout(d *Dep) error {
@@ -41,27 +80,20 @@ func (g Git) Checkout(d *Dep) error {
 	return cmd.Run()
 }
 
+func (g Git) Fetch(path string) error {
+	return runInPath(path, func() error {
+		return exec.Command("git", "fetch").Run()
+	})
+}
+
 type Hg struct{}
 
-// TODO someone should vet this that knows hg
 func (h Hg) Init(d *Dep) error {
-	path := fmt.Sprintf("%s/s", VendorDir, d.Import)
-	if err := os.MkdirAll(path, 0755); err != nil {
-		return err
-	} else {
-		if _, err := os.Stat(fmt.Sprintf("%s/%s", path, ".hg")); os.IsNotExist(err) {
-			cmd := exec.Command("hg", "clone", d.Source, path)
-			if err := cmd.Run(); err != nil {
-				return err
-			}
-		} else if err == nil {
+	return initScm(d, ".hg", h)
+}
 
-			log.Printf("Hg dir exists for %s, skipping clone. To reset the source, run `rm -R %s`, then run gopack again", d.Import, path)
-		} else {
-			return fmt.Errorf("Error while examining hg dir for %s: %s", d.Import, err)
-		}
-	}
-	return nil
+func (h Hg) DownloadCommand(source, path string) *exec.Cmd {
+	return exec.Command("hg", "clone", source, path)
 }
 
 func (h Hg) Checkout(d *Dep) error {
@@ -76,13 +108,22 @@ func (h Hg) Checkout(d *Dep) error {
 	return cmd.Run()
 }
 
+func (h Hg) Fetch(path string) error {
+	return runInPath(path, func() error {
+		return exec.Command("hg", "pull").Run()
+	})
+}
+
 type Svn struct {
 }
 
 // FIXME someone that has an SVN repo accessible, please
 func (s Svn) Init(d *Dep) error {
-	failf("SVN repos not yet fully supported")
-	return nil
+	return initScm(d, ".svn", s)
+}
+
+func (s Svn) DownloadCommand(source, path string) *exec.Cmd {
+	return exec.Command("svn", "checkout", source, path)
 }
 
 func (s Svn) Checkout(d *Dep) error {
@@ -100,6 +141,12 @@ func (s Svn) Checkout(d *Dep) error {
 	return cmd.Run()
 }
 
+func (s Svn) Fetch(path string) error {
+	return runInPath(path, func() error {
+		return exec.Command("svn", "update").Run()
+	})
+}
+
 // The Go provider embeds another provider and only implements Init so that
 // deps that don't specify a provider keep working like they did before
 type Go struct {
@@ -107,6 +154,9 @@ type Go struct {
 }
 
 func (g Go) Init(d *Dep) error {
-	cmd := exec.Command("go", "get", "-d", "-u", d.Import)
-	return cmd.Run()
+	return g.DownloadCommand(d.Import, "").Run()
+}
+
+func (g Go) DownloadCommand(source, path string) *exec.Cmd {
+	return exec.Command("go", "get", "-d", "-u", source)
 }
